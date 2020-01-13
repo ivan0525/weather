11a9216ff1ca9c1d3c20c0ac95144c0c25208e98
solophone <solophone@aliyun.com>
2020-01-13 18:02:41 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'react-native/no-inline-styles': 0,
+    '@typescript-eslint/no-unused-vars': 1
   }
 };
