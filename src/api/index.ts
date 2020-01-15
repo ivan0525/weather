143cbefd665d296cf4cc3d4e6b63fb2f2d02ebea
solophone <solophone@aliyun.com>
2020-01-15 10:19:38 +0800

    添加城市接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,21 @@
 import request from 'axios'
 import { WEB_KEY } from '../config/index'
-const BASE_URL = 'https://free-api.heweather.net/s6/weather'
+const WEATHER_BASE_URL = 'https://free-api.heweather.net/s6/weather'
+const CITY_BASE_URL = 'https://search.heweather.net'
 export const getNowWeather = (params: any) => {
   params = { ...params, ...{ key: WEB_KEY } }
   return request({
-    url: `${BASE_URL}/now`,
+    url: `${WEATHER_BASE_URL}/now`,
+    method: 'GET',
+    params
+  })
+}
+
+// 通过关键字查询城市（支持模糊查询）
+export const getCityByKeyWord = (params: any) => {
+  params = { ...params, ...{ key: WEB_KEY } }
+  return request({
+    url: `${CITY_BASE_URL}/find`,
     method: 'GET',
     params
   })
